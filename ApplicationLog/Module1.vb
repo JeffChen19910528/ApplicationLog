60e79d4a026c5e240069ac6797bb62ce6a3bfdab
Hsinchueh Chen 陳新爵 <1709002@iisigroup.com>
2020-07-24 18:22:01 +0800

    加入專案檔案。

---
+++ ApplicationLog/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        TracingTest("LogFile.txt")
+        Console.ReadLine()
+    End Sub
+
+    Public Sub TracingTest(ByVal fileName As String)
+        My.Application.Log.WriteEntry($"Entering TracingTest with argument '{fileName}'.")
+
+        ' Code to trace goes here.
+
+        My.Application.Log.WriteEntry($"Exiting TracingTest with argument '{fileName}'.")
+    End Sub
+
+End Module
